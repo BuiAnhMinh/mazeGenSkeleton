db988ab1f39ca21b21f3f064eec4c664a1f43754
hung9988 <Hung.LNQ225975@sis.hust.edu.vn>
2024-08-15 22:49:46 +0700

    i cooked

--- maze/edgeListGraph.py
+++ maze/edgeListGraph.py
@@ -21,67 +21,85 @@ class EdgeListGraph(Graph):
 
     def __init__(self):
         ### Implement me! ###
-        pass
-
+        self.vertices_dict:dict[list]={}
+        self.edge_list:dict[tuple:bool]={}
 
         
     def addVertex(self, label:Coordinates):
         ### Implement me! ###
-        pass
-
-
+        self.vertices_dict[label]=[]
+        
+   
 
     def addVertices(self, vertLabels:List[Coordinates]):
         ### Implement me! ###
-        pass
-
+        for vertex in vertLabels:
+            self.addVertex(vertex)
+   
 
 
     def addEdge(self, vert1:Coordinates, vert2:Coordinates, addWall:bool = False)->bool:
         ### Implement me! ###
         # remember to return booleans
-        pass
         
+        if not self.hasEdge(vert1, vert2):
+            self.edge_list[(vert1, vert2)] = addWall
+            self.edge_list[(vert2, vert1)] = addWall
+            self.vertices_dict[vert1].append(vert2)
+            self.vertices_dict[vert2].append(vert1)
+            return True
+        else:
+            return False
+   
+     
 
 
     def updateWall(self, vert1:Coordinates, vert2:Coordinates, wallStatus:bool)->bool:
         ### Implement me! ###
         # remember to return booleans
-        pass
-
-
-
+        if self.hasEdge(vert1, vert2):
+            self.edge_list[(vert1, vert2)] = wallStatus
+            self.edge_list[(vert2, vert1)] = wallStatus
+            return True
+        return False
+        
+        
     def removeEdge(self, vert1:Coordinates, vert2:Coordinates)->bool:
         ### Implement me! ###
         # remember to return booleans
-        pass
-        
+        if self.hasEdge(vert1, vert2):
+            self.vertices_dict[vert1].remove(vert2)
+            self.vertices_dict[vert2].remove(vert1)
+            del self.edge_list[(vert1, vert2)]
+            del self.edge_list[(vert2, vert1)]
+            return True
 
 
     def hasVertex(self, label:Coordinates)->bool:
         ### Implement me! ###
         # remember to return booleans
-        pass
+        if label in self.vertices_dict.keys():
+            return True
+        return False
+
 
 
 
     def hasEdge(self, vert1:Coordinates, vert2:Coordinates)->bool:
         ### Implement me! ###
         # remember to return booleans
-        pass
-
-
+        if (vert1, vert2) in self.edge_list.keys():
+                return True
+        return False
 
     def getWallStatus(self, vert1:Coordinates, vert2:Coordinates)->bool:
         ### Implement me! ###
         # remember to return booleans
-        pass
+        return self.edge_list[(vert1, vert2)]
         
     
-
-
     def neighbours(self, label:Coordinates)->List[Coordinates]:
         ### Implement me! ###
         # remember to return list of coordinates
-        pass
+        return self.vertices_dict[label]
         
